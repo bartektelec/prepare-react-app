b364300982ad749444ad8ff37b374a53c053c9b5
bartektelec <bartektelec@gmail.com>
2021-02-11 20:32:00 +0100

    feat: :construction: add a react base blueprint with snowpack typescript and jest

---
+++ src/blueprints/basets/.prettierrc.js
@@ -0,0 +1,10 @@
+exports.default = {
+  trailingComma: "es5",
+  endOfLine: "lf",
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: true,
+  arrowParens: "avoid",
+};
