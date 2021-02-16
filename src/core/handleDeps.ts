528190b33492e4bbc3fbef6279aa225315f087b8
bartektelec <bartektelec@gmail.com>
2021-02-16 16:56:15 +0100

    feat: :sparkles: add feature for copying blueprint files

--- src/core/handleDeps.ts
+++ src/core/handleDeps.ts
@@ -4,3 +4,7 @@
 // SECTION
 // Input: Features list
 // Return: Dependency files - package.json (and optional deps.js file if needed)
+
+import { Feature } from '../consts/features';
+
+export default async function (features: Feature[]) {}
